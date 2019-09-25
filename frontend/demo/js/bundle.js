af705730219aae0f5c759a06da884ca7d1705f64
Chetan Surpur <chetan51@gmail.com>
2019-09-25 15:08:19 -0700

    Show all models at once

--- frontend/demo/js/bundle.js
+++ frontend/demo/js/bundle.js
@@ -11202,55 +11202,88 @@ class Plot {
     // initialize properties
     this.timestep = 0;
     this.layerIndex = 0;
+    this.currentModelData = {};
   }
 
   load() {
-    // load a data point
-    let path = `data/layer-${this.layerIndex}_timestep-${this.timestep}.png`;
-    $.ajax({
-      url: path,
-      xhrFields: {
-        responseType: 'arraybuffer'
-      },
-      success: (result) => {
-        // parse it as a PNG
-        var reader = new PNGReader(result);
-        reader.parse((err, png) => {
-          if (err) throw err;
+    let requests = [];
 
-          let width = png["width"];
-          let height = png["height"];
+    // create requests 
+    for (let model of this.models) {
+      let path = `data/${model}/layer-${this.layerIndex}_timestep-${this.timestep}.png`;
+      requests.push(
+        $.ajax({
+          url: path,
+          xhrFields: {
+            responseType: 'arraybuffer'
+          },
+          success: (result) => {
+            // parse it as a PNG
+            var reader = new PNGReader(result);
+            return reader.parse((err, png) => {
+              if (err) throw err;
 
-          var z = [];
-          
-          for (var i = 0; i < height; i++) {
-            z[i] = [];
-            for (var j = 0; j < width; j++) {
-              z[i][j] = png["pixels"][(i * width) + j] / 255;
-            }
+              this.currentModelData[model] = png;
+            });
+          },
+          error: (err) => {
+            throw err;
           }
+        })
+      );
+    }
 
-          var data = [
-            {
-              z: z,
-              type: 'heatmap',
-              zmin: 0,
-              zmax: 1,
-              zauto: false
-            }
-          ];
+    // load the current timestep for each model
+    $.when(
+      ...requests
+    ).then(() => {
+      let traces = [];
+      let modelIndex = 0;
 
-          if (this.timestep == 0) {
-            Plotly.plot(this.plotElement, data);
+      for (let [model, data] of Object.entries(this.currentModelData)) {
+        let width = data["width"];
+        let height = data["height"];
+
+        var z = [];
+        
+        for (var i = 0; i < height; i++) {
+          z[i] = [];
+          for (var j = 0; j < width; j++) {
+            z[i][j] = data["pixels"][(i * width) + j] / 255;
           }
-          else {
-            // Plotly.restyle(plotElement, { z: [data["z"]] });
-            Plotly.newPlot(this.plotElement, data);
-          }
-        });
-      },
-      error: (err) => {
-        throw err;
+        }
+
+        var trace = {
+          z: z,
+          type: 'heatmap',
+          zmin: 0,
+          zmax: 1,
+          zauto: false,
+          xaxis: `x${modelIndex+1}`,
+          yaxis: `y${modelIndex+1}`,
+        };
+
+        traces.push(trace);
+
+        modelIndex++;
+      };
+
+      var layout = {
+        height: 900,
+        grid: {
+          rows: this.models.length,
+          columns: 1,
+          pattern: 'independent',
+        },
+      };
+
+      if (this.timestep == 0) {
+        Plotly.plot(this.plotElement, traces, layout);
+        // Plotly.plot(this.plotElement, [traces[0]]);
+      }
+      else {
+        // Plotly.restyle(plotElement, { z: [data["z"]] });
+        Plotly.newPlot(this.plotElement, traces, layout);
       }
     });
   }
